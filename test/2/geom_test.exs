0dd1d1bda8095076ae37053d1ecba3632a275d19
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-15 07:48:44 +1200

    Initial commit

---
+++ test/2/geom_test.exs
@@ -0,0 +1,19 @@
+defmodule Etudes.GeomTest do
+  use ExUnit.Case
+
+  test "rectangle area" do
+    assert Etudes.Geom.area(3, 4) == 12
+  end
+
+  test "bigger rectangle area" do
+    assert Etudes.Geom.area(12, 7) == 81
+  end
+
+  test "defaults to rectangle of width 1" do
+    assert Etudes.Geom.area(7) == 7
+  end
+
+  test "defaults to square of 1x1" do
+    assert Etudes.Geom.area() == 1
+  end
+end
