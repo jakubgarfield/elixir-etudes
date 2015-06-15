a14b10256b600223993c851b2b8f4123d559db73
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-16 07:46:05 +1200

    Refactor area parameters into tuple

--- lib/2/geom.ex
+++ lib/2/geom.ex
@@ -4,30 +4,26 @@ defmodule Etudes.Geom do
   """
 
   @doc """
-  Calculates the area of the rectangular shape
-  """
-  @spec area(number(), number()) :: number()
-  def area(height \\ 1, width \\ 1) do
-    height * width
-  end
-
-  @doc """
   Calculates the area of a selected shape
   """
-  @spec area(atom, number, number) :: number
-  def area(:rectangle, height, width) when height >= 0 and width >= 0 do
+  @spec area({atom, number, number}) :: number
+  def area({shape, a, b}) do
+    area(shape, a, b)
+  end
+
+  defp area(:rectangle, height, width) when height >= 0 and width >= 0 do
     area(height, width)
   end
 
-  def area(:triangle, a, b) when a >= 0 and b > 0 do
+  defp area(:triangle, a, b) when a >= 0 and b > 0 do
     (a * b) / 2
   end
 
-  def area(:ellipse, a, b) when a >= 0 and b >= 0 do
+  defp area(:ellipse, a, b) when a >= 0 and b >= 0 do
     :math.pi * a * b
   end
 
-  def area(_, _, _) do
+  defp area(_, _, _) do
     0
   end
 end
--- test/2/geom_test.exs
+++ test/2/geom_test.exs
@@ -1,47 +1,30 @@
 defmodule Etudes.GeomTest do
   use ExUnit.Case
-
-  test "rectangle area" do
-    assert Etudes.Geom.area(3, 4) == 12
-  end
-
-  test "bigger rectangle area" do
-    assert Etudes.Geom.area(12, 7) == 84
-  end
-
-  test "defaults to rectangle of width 1" do
-    assert Etudes.Geom.area(7) == 7
-  end
-
-  test "defaults to square of 1x1" do
-    assert Etudes.Geom.area() == 1
-  end
-
   test "triangle area" do
-    assert Etudes.Geom.area(:triangle, 3, 5) == 7.5
+    assert Etudes.Geom.area({:triangle, 3, 5}) == 7.5
   end
 
   test "rectangle area" do
-    assert Etudes.Geom.area(:rectangle, 3,4) == 12
+    assert Etudes.Geom.area({:rectangle, 3, 4}) == 12
   end
 
   test "ellipse area" do
-    assert Etudes.Geom.area(:ellipse, 2,4) == 25.132741228718345
+    assert Etudes.Geom.area({:ellipse, 2, 4}) == 25.132741228718345
   end
 
   test "negative length for triangle" do
-    assert Etudes.Geom.area(:triangle, -3, 2) == 0
+    assert Etudes.Geom.area({:triangle, -3, 2}) == 0
   end
 
   test "negative b for ellipse" do
-    assert Etudes.Geom.area(:ellipse, 1, -2) == 0
+    assert Etudes.Geom.area({:ellipse, 1, -2}) == 0
   end
 
   test "negative sides for rectangle" do
-    assert Etudes.Geom.area(:rectangle, -1, -1) == 0
+    assert Etudes.Geom.area({:rectangle, -1, -1}) == 0
   end
 
   test "return 0 for not supported type" do
-    assert Etudes.Geom.area(:pentagon, 2, 5) == 0
+    assert Etudes.Geom.area({:pentagon, 2, 5}) == 0
   end
 end
