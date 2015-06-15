f8bd93f2bb080afb30ca611021034123c8e9bdb1
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-16 07:07:53 +1200

    Add documentation to Geom module

--- lib/2/geom.ex
+++ lib/2/geom.ex
@@ -1,4 +1,12 @@
 defmodule Etudes.Geom do
+  @moduledoc """
+  Provides geometrical functions
+  """
+
+  @doc """
+  Calculates the area of the rectangular shape
+  """
+  @spec area(number(), number()) :: number()
   def area(height \\ 1, width \\ 1) do
     height * width
   end
