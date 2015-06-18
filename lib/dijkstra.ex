98677c626b4fd99b517aa0abeacbd1fc261f0f1f
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-19 08:02:09 +1200

    Add Dijkstra's gcd

---
+++ lib/dijkstra.ex
@@ -0,0 +1,22 @@
+defmodule Dijkstra do
+  @moduledoc """
+  A module with all the great mathematic algorithms from Dijkstra
+  """
+
+  @doc """
+  Calculates the greatest common divisor of two numbers using Dijkstra
+  method
+  """
+  @spec gcd(number, number) :: number
+  def gcd(x, y) when x == y do
+    x
+  end
+
+  def gcd(x, y) when x > y do
+    gcd(x - y, y)
+  end
+
+  def gcd(x, y) do
+    gcd(x, y - x)
+  end
+end
---
+++ test/dijkstra_test.exs
@@ -0,0 +1,15 @@
+defmodule DijkstraTest do
+  use ExUnit.Case
+
+  test "gcd of the same number should equal that number" do
+    assert Dijkstra.gcd(8, 8) == 8
+  end
+
+  test "no common divisor apart from 1" do
+    assert Dijkstra.gcd(125, 146) == 1
+  end
+
+  test "one of the numbers is greater and they share a common divisor greater than 1" do
+    assert Dijkstra.gcd(120, 36) == 12
+  end
+end
