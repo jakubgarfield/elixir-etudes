c8ae7c3da74a57ad98592588fccf1cf8d00194c1
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-20 07:59:35 +1200

    Add tail recursion version of pow

--- lib/powers.ex
+++ lib/powers.ex
@@ -23,4 +23,34 @@ defmodule Etudes.Powers do
   def pow(x, e) when e < 0 do
     1.0 / pow(x, -e)
   end
+
+  @doc """
+  Raises the number but this time it doesn't blow the stack
+  """
+  @spec pow_tail(number, number) :: number
+
+  def pow_tail(_, 0) do
+    1
+  end
+
+  def pow_tail(x, 1) do
+    x
+  end
+
+  def pow_tail(x, e) when e > 1 do
+    pow_tail(x, e, 1)
+  end
+
+  def pow_tail(x, e) when e < 0 do
+    1.0 / pow_tail(x, -e)
+  end
+
+  @spec pow_tail(number, number, number) :: number
+  defp pow_tail(_, 0, acc) do
+    acc
+  end
+
+  defp pow_tail(x, e, acc) do
+    pow_tail(x, e - 1, x * acc)
+  end
 end
--- test/powers_test.exs
+++ test/powers_test.exs
@@ -17,4 +17,22 @@ defmodule Etudes.PowersTest do
   test "returns for negative exponent" do
     assert Powers.pow(2, -3) == 0.125
   end
+
+
+
+  test "#pow_tail returns 1 when exponent is 0" do
+    assert Powers.pow_tail(10, 0) == 1
+  end
+
+  test "#pow_tail returns x when exponent is 1" do
+    assert Powers.pow_tail(10, 1) == 10
+  end
+
+  test "#pow_tail returns for positive exponent" do
+    assert Powers.pow_tail(2, 3) == 8
+  end
+
+  test "#pow_tail returns for negative exponent" do
+    assert Powers.pow_tail(2, -3) == 0.125
+  end
 end
