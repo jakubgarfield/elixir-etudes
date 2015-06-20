92716115ceadfcfd734b025828ad22bf3e59db00
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-20 22:00:47 +1200

    Add julian to dates module that tells the number of days for date

--- lib/dates.ex
+++ lib/dates.ex
@@ -11,4 +11,30 @@ defmodule Etudes.Dates do
     string_parts = String.split(iso_date, "-")
     Enum.map(string_parts, fn(part) -> String.to_integer(part) end)
   end
+
+
+  @spec julian(String.t) :: number
+  def julian(iso_date) do
+    [year, month, day] = date_parts(iso_date)
+    julian(day, month, days_in_year(year))
+  end
+
+  defp julian(total, month, [number_of_days_in_current_month | rest_of_the_year]) when month > 1 do
+    julian(total + number_of_days_in_current_month, month - 1, rest_of_the_year)
+  end
+
+  defp julian(total, _, _) do
+    total
+  end
+
+  defp days_in_year(year) do
+    cond do
+      is_leap_year(year) -> [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      true -> [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    end
+  end
+
+  defp is_leap_year(year) do
+    (rem(year,4) == 0 and rem(year,100) != 0) or (rem(year, 400) == 0)
+  end
 end
--- test/dates_test.exs
+++ test/dates_test.exs
@@ -3,6 +3,23 @@ defmodule Etudes.DatesTest do
   alias Etudes.Dates
 
   test "#date_parts parses date parts" do
-    assert Dates.date_parts("2014-03-12") === [2014, 3, 12]
+    assert Dates.date_parts("2014-03-12") == [2014, 3, 12]
+  end
+
+
+  test "#julian works for non leap years" do
+    assert Dates.julian("2014-12-31") == 365
+  end
+
+  test "#julian works for leap year" do
+    assert Dates.julian("2012-12-31") == 366
+  end
+
+  test "#julian works for all the test samples" do
+    assert Dates.julian("2012-02-05") == 36
+    assert Dates.julian("2013-02-05") == 36
+    assert Dates.julian("1900-03-01") == 60
+    assert Dates.julian("2000-03-01") == 61
+    assert Dates.julian("2013-01-01") == 1
   end
 end
