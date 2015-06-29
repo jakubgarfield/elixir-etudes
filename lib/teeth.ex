cac1f3f87df7816176947196898b4e809debcf24
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2015-06-29 14:58:02 +0200

    Add teeth pocket generator

--- lib/teeth.ex
+++ lib/teeth.ex
@@ -23,4 +23,51 @@ defmodule Etudes.Teeth do
   defp alert([], result, _) do
     result
   end
+
+  def generate_pockets(list_of_teeth, probability) do
+    :random.seed(:erlang.now())
+    generate_pockets(list_of_teeth, probability, [])
+  end
+
+  defp generate_pockets([head | tail], probability, accumulator) do
+    if head == ?T do
+      generate_pockets(tail, probability, accumulator ++ [generate_tooth(probability)])
+    else
+      generate_pockets(tail, probability, accumulator ++ [0])
+    end
+  end
+
+  defp generate_pockets([], _, accumulator) do
+    accumulator
+  end
+
+  defp generate_tooth(probability) do
+    if (:random.uniform < probability) do
+      generate_tooth(2, 6, [])
+    else
+      generate_tooth(3, 6, [])
+    end
+  end
+
+  defp generate_tooth(_, 0, acc) do
+    acc
+  end
+
+  defp generate_tooth(base, n, acc) do
+    delta = :random.uniform(3) - 2
+    generate_tooth(base, n - 1, acc ++ [base + delta])
+  end
+
+  def test_pockets() do
+    tlist = 'FTTTTTTTTTTTTTTFTTTTTTTTTTTTTTTT'
+    big_list = generate_pockets(tlist, 0.75)
+    print_pockets(big_list)
+  end
+
+  defp print_pockets([]), do: IO.puts("Finished.")
+
+  defp print_pockets([head | tail]) do
+    IO.puts(inspect(head))
+    print_pockets(tail)
+  end
 end
